9e0ac17dd6e5aa01c1f84137615e6e079d0a8266
eugene <ghelius@gmail.com>
2014-03-11 14:50:21 +0700

    add pythons script, change window_pid, now it's run by cron

--- climat_control.sh
+++ climat_control.sh
@@ -1,122 +1,79 @@
 #!/bin/bash
 #set -x
-
-Tsp=25
-
+PID_DIR="/home/eugene/.window_climat_control_pid"
+PORT=/dev/ttyUSB0
 #PI coefficients
 P_=20
 I_=10
 D_=20
+#Other saved value
 OUT_MAX=100
-P_=`cat /home/eugene/.climat_PI_P_coeff`
-I_=`cat /home/eugene/.climat_PI_I_coeff`
-D_=`cat /home/eugene/.climat_PI_D_coeff`
-Intgr=`cat /home/eugene/.climat_integrator_saved`
-OUT_MAX=`cat /home/eugene/.climat_OUT_max`
-PrevValue=`cat /home/eugene/.climat_T_setpoint`
-ITERATES=12 # number of received temperature points (about 5 minuts)
-#ITERATES=1 # number of received temperature points (about 5 minuts)
+P_=`cat "$PID_DIR"/climat_PI_P_coeff`
+I_=`cat "$PID_DIR"/climat_PI_I_coeff`
+D_=`cat "$PID_DIR"/climat_PI_D_coeff`
+Intgr=`cat "$PID_DIR"/climat_integrator_saved`
+OUT_MAX=`cat "$PID_DIR"/climat_OUT_max`
+Tpv=`cat "$PID_DIR"/real_tempr`
+Tsp=`cat "$PID_DIR"/climat_T_setpoint`
+PrevValue=`cat "$PID_DIR"/climant_last_value`
 
-echo "P=$P_, I=$I_, D=$D_, OutMax=$OUT_MAX"
-CNT=0
-PORT=/dev/ttyUSB0
-Tpv_filter_summ=0
-miniterm.py --lf -qp  /dev/ttyUSB0 -b 19200 | while read Tpv_raw; do
-	Tpv_raw=$(echo $Tpv_raw | egrep -o '[0-9.]+') #avoid bad symbols
-	echo "received: [$Tpv_raw]"
-	Tpv_filter_summ=$(echo "$Tpv_filter_summ + $Tpv_raw" | bc -l)
-	if (( CNT < $ITERATES )); then
-		let CNT++
-		continue
-	fi
-	Tpv=$(echo "$Tpv_filter_summ/($ITERATES+1)" | bc -l)
-	CNT=0
-	Tpv_filter_summ=0;
-	echo "filter: $Tpv"
-	Tsp=`cat /home/eugene/.climat_T_setpoint`
+echo "Real value:$Tpv, P=$P_, I=$I_, D=$D_, OutMax=$OUT_MAX"
+Error=$(echo "($Tpv-$Tsp)" | bc -l)
+Diff=$(echo "($Tpv-$PrevValue)" | bc -l)
+echo "$Tpv" > "$PID_DIR"/climant_last_value
 
-	Error=$(echo "($Tpv-$Tsp)" | bc -l)
-	Diff=$(echo "($Tpv-$PrevValue)" | bc -l)
-	PrevValue=$Tpv
-
-#trancate old integrator history
-#	echo "$Error" >> /home/eugene/.climat_integrator_saved
-#	echo -e "$(tac .climat_integrator_saved | head -n$I_ | tac)" > .climat_integrator_saved
-
-	echo "Tsp=$Tsp, Tpv=[31m$Tpv[0m, Err=$Error"
-
-# calc intergation value
-#	integrator_cnt=0
-#	Intgr=0
-#	while read _err 
-#	do
-	#	echo "error was: $_err"
-#		Intgr=$(echo "($Intgr + ($_err))" | bc -l)
-#		let integrator_cnt++
-#		if ((integrator_cnt > I_ ))
-#		then
-#			break
-#		fi
-#	done < /home/eugene/.climat_integrator_saved
-#	echo "Integrator value: $Intgr"
-	
-	Intgr=$(echo "$Intgr+($Error*$I_)" | bc -l)
-	echo "calc new Intgr: $Intgr"
-
-	tmpIntgr=$(echo "$Intgr" | egrep -o '^[-0-9]+')
-	echo "prepared value: $tmpIntgr"
-
-#	if [ -z "$tmpIntgr" ]; then
-#		Intgr=0
-#	fi
-
-	if (( tmpIntgr > 100 )); then
-		Intgr=100
-	fi
-	if (( tmpIntgr < 0 )); then
-		Intgr=0
-	fi
-	
-	
-	echo "$Intgr" > /home/eugene/.climat_integrator_saved
+echo "Tsp=$Tsp, Tpv=[31m$Tpv[0m, Err=$Error"
 
 	
-	Iout="$Intgr"
-	Pout=$(echo "$Error*$P_" | bc -l)
-	Dout=0 #$(echo "$Diff*($D_)" | bc -l)
-	
+Intgr=$(echo "$Intgr+($Error*$I_)" | bc -l)
+echo "calc new Intgr: $Intgr"
 
-	ZeroSing=$(echo $Error | grep -o '[-]')
-	OUT_RAW=$(echo "(($Iout)+($Pout)+($Dout))" | bc -l)
-	OUT=$(echo "10+(($OUT_RAW)*$OUT_MAX)/100" | bc -l)
-	echo "out: $OUT"
-	OUT=$(echo "$OUT" | egrep -o '^[-0-9]+')
+tmpIntgr=$(echo "$Intgr" | egrep -o '^[-0-9]+')
+echo "prepared value: $tmpIntgr"
 
-	if [ "$OUT" == "" ]; then
-		OUT=0
-	fi
-	if [ "$OUT" == "-" ]; then
-		OUT=0
-	fi
-
-	if (( OUT < 10 )); then
-		OUT=10
+if (( tmpIntgr > 100 )); then
+	Intgr=100
+fi
+if (( tmpIntgr < 0 )); then
+	Intgr=0
+fi
 		
-	fi
-	if (( OUT > 100 )); then
-		OUT=100
-	fi
+echo "$Intgr" > "$PID_DIR"/climat_integrator_saved
 
-	echo "P=$Pout, I=$Iout, D=$Dout(disable), OUT=$OUT_RAW [real=[32m$OUT[0m, max=$OUT_MAX]"
+	
+Iout="$Intgr"
+Pout=$(echo "$Error*$P_" | bc -l)
+Dout=0 #$(echo "$Diff*($D_)" | bc -l)
 
-	echo "$OUT;">$PORT
+OUT_RAW=$(echo "(($Iout)+($Pout)+($Dout))" | bc -l)
+OUT=$(echo "10+(($OUT_RAW)*$OUT_MAX)/100" | bc -l)
+echo "out: $OUT"
+OUT=$(echo "$OUT" | egrep -o '^[-0-9]+')
 
-	TpvFixPoint=$(echo "$Tpv*100" | bc -l | egrep -o '^[0-9]+') 
-	TspFixPoint=$(echo "$Tsp*100" | bc -l | egrep -o '^[0-9]+') 
-	echo "update db:$TpvFixPoint:$TspFixPoint:$OUT:$Pout:$Iout:$Dout"
-	rrdtool update /home/eugene/db/climatc.rrd `date +"%s"`:$TpvFixPoint:$TspFixPoint:$OUT:$Pout:$Iout:$Dout
-	echo "send $Tpv to narodmon"
-	echo "$Tpv" > /home/eugene/.narodmon.data/atom/indoor_tempr/value
+if [ "$OUT" == "" ]; then
+	OUT=0
+fi
+if [ "$OUT" == "-" ]; then
+	OUT=0
+fi
+
+if (( OUT < 10 )); then
+	OUT=10
+	
+fi
+if (( OUT > 100 )); then
+	OUT=100
+fi
+
+echo "P=$Pout, I=$Iout, D=$Dout(disable), OUT=$OUT_RAW [real=[32m$OUT[0m, max=$OUT_MAX]"
+
+echo "$OUT;">$PORT
+
+TpvFixPoint=$(echo "$Tpv*100" | bc -l | egrep -o '^[0-9]+') 
+TspFixPoint=$(echo "$Tsp*100" | bc -l | egrep -o '^[0-9]+') 
+echo "update db:$TpvFixPoint:$TspFixPoint:$OUT:$Pout:$Iout:$Dout"
+
+rrdtool update /home/eugene/db/climatc.rrd `date +"%s"`:$TpvFixPoint:$TspFixPoint:$OUT:$Pout:$Iout:$Dout
 
 rrdtool graph /var/www/rrd_graph/climat_t.png \
 	--vertical-label C \
@@ -214,7 +171,6 @@ rrdtool graph /var/www/rrd_graph/climat_w_48.png \
 	DEF:i=/home/eugene/db/climatc.rrd:i:AVERAGE LINE2:i#00FF00:I \
 	DEF:d=/home/eugene/db/climatc.rrd:d:AVERAGE LINE2:d#0000FF:D 
 
-done
 
 
 
--- create_system_rrd_database.sh
+++ create_system_rrd_database.sh
@@ -2,15 +2,13 @@
 
 START=`date +"%s"`
 
-rrdtool create climatc.rrd \
+rrdtool create climat.rrd \
   --start $START \
-  --step 300 \
-  DS:tempr:GAUGE:700:1000:3500 \
-  DS:sp:GAUGE:700:1000:3500 \
-  DS:hole:GAUGE:700:0:100 \
-  DS:p:GAUGE:700:0:100 \
-  DS:i:GAUGE:700:0:100 \
-  DS:d:GAUGE:700:0:100 \
-	RRA:AVERAGE:0.5:1:400 \
+  --step 60 \
+  DS:tempr:GAUGE:120:0:35 \
+  DS:tempr_sp:GAUGE:120:0:35 \
+  DS:humid:GAUGE:120:0:100 \
+  DS:humid_sp:GAUGE:120:0:100 \
+	RRA:MAX:0.5:1:1500 \
 	RRA:AVERAGE:0.5:10:900 \
-	RRA:AVERAGE:0.5:6:1000 
+	RRA:AVERAGE:0.5:10:1000 
